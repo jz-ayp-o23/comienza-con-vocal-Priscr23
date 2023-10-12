7ea2331411cd54507a6543d493560147c39da4a0
Priscr23 <Priscila.cervantes@iteso.mx>
2023-10-11 18:12:21 -0600

    1

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -1,15 +1,14 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+ Diseña un programa para determinar si una palabra o frase comienza con una vocal. Toma en cuenta que las vocales no por estar en mayúscula o tener acento dejan de ser vocales.
+ 750722
 """
+#Entradas
+palabra = []
+palabra = input("Escribe una palabra: ")
+palabra = palabra.lower()
 
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+#Procesos
+if palabra[0] in ["a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú"]:
+    print(f"'{palabra}' comienza con vocal")
+else:
+    print(f"'{palabra}' no comienza con vocal")
